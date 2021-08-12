53012641fa38f2d51deabb210b9f9590b01e5b2a
Chaowei Xiao <xiaocw@umich.edu>
2021-08-12 16:03:25 -0400

    chaange

--- submit_ngc.sh
+++ submit_ngc.sh
@@ -3,4 +3,4 @@ pip install torch==1.6.0 torchvision==0.7.0
 cp /workspace/imgnet.sh /tmp;
 cp /dataset/*.tar /tmp;
 cd /tmp && sh imgnet.sh;
-python train_imagenet.py --data /tmp/ -save /workspace/edge/imagenet_tiny2 --train_batch 1024
+python train_imagenet.py --data /tmp/ -save /workspace/edge/imagenet_tiny2 --train_batch 1024 --lr 0.2
